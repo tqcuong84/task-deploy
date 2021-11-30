dd23f3dfec862717a1e17f15255b54ae69feff8a
quoccuong <tqcuong1984@gmail.com>
2021-11-30 23:37:27 +0700

    upload source

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,23 +9,4 @@ class DashboardController extends Controller
         $info = session('user_login');
         return view('dashboard', compact('info'));
     }
-    public function deploy(){
-        $path = base_path('.env.example');
-        if (file_exists($path)) {
-            $final_env_content = str_replace('LARAVEL_DB', 'task-app2', file_get_contents($path));
-            $final_env_content = str_replace('LARAVEL_USER_DB', 'root', $final_env_content);
-            $final_env_content = str_replace('LARAVEL_PWD_DB', 'root', $final_env_content);
-            file_put_contents(base_path('.env.copy'), $final_env_content);
-        }
-
-        $process = new Process(['/var/www/task-app1/deploy.sh']);
-        $process->run();
-
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        return redirect()->away("http://alt.plantactic.io");
-    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,6 @@
         <div class="card">
             <div class="card-body text-center">
                 Welcome<br><strong class="text-larger">{{$info['name']}}</strong><br /><br><br>
-                <form action="/deploy" method="post">
-                <button type="submit" class="btn btn-primary">Deploy</button>
-                    {{ csrf_field() }}
-                </form><br><br>
                 <a href="/signout" class="btn btn-link btn-sm">Signout</a>
             </div>
         </div>
